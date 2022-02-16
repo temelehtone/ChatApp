f349824c311e3b09da6e614ab8eed7d54432f1c0
temelehtone <temelehtone@gmail.com>
2022-02-16 23:12:05 +0200

    templates setup

--- website/main.py
+++ website/main.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template, url_for
+from django.shortcuts import redirect
+from flask import Flask, render_template, session, url_for, request
+
+NAME_KEY = 'name'
 
 app = Flask(__name__)
+app.secret_key = "helloimteuvo"
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    #session.pop(NAME_KEY, None)
+    return redirect(url_for("login"))
 
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template()
+    # if NAME_KEY not in session:
+    #     return redirect(url_for("login"))
+
+    #name = session[NAME_KEY]
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
