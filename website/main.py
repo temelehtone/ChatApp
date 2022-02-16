b310eb6b462ff5380bcb0520348096a558504642
temelehtone <temelehtone@gmail.com>
2022-02-17 01:42:24 +0200

    showing message and scroll works fine

--- website/main.py
+++ website/main.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask, render_template, session, url_for, request, redirect
 from client import Client
 
@@ -8,10 +9,12 @@ app.secret_key = "helloimteuvo"
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
+    session["messages"] = []
     if request.method == "POST":
         session[NAME_KEY] = request.form["inputName"]
+        
         return redirect(url_for("home"))
-    return render_template("login.html")
+    return render_template("login.html", **{"session": session})
 
 
 @app.route("/logout")
@@ -20,20 +23,25 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["POST", "GET"])
+@app.route("/home", methods=["POST", "GET"])
 def home():
+    
     if NAME_KEY not in session:
         return redirect(url_for("login"))
+    if request.method == "POST":
+        session["messages"].extend(request.form["message"])
 
-    name = session[NAME_KEY]
 
-    return render_template("index.html")
 
-@app.route("/run")
-def run():
-    print("RUN")
-    return "nice"
+    return render_template("index.html", **{"login":True, "session": session, "messages": session["messages"]})
+
+#@app.route("/add_msg", methods=["POST", "GET"])
+# def run():
+    
+#         return render_template("index.html", **{"login":True, "session": session, "messages": session["messages"]})
+    
+#     return render_template("index.html", **{"login":True, "session": session, "messages": session["messages"]})
 
 
 if __name__ == "__main__":
--- website/static/index.js
+++ website/static/index.js
@@ -1,13 +1,20 @@
+
+
+$('.text-field').scrollTop($('.text-field')[0].scrollHeight);
+
+
+
 $(function() {
     $('#test').on('click', function(e) {
       e.preventDefault()
-      $.getJSON('/run',
+      $.getJSON('/add_msg',
           function(data) {
         //do nothing
       });
       return false;
     });
   });
+
 
 function validate(name) {
     if (name.length >= 2) {
